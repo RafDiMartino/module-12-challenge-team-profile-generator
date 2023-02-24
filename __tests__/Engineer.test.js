e8219c0ec14cd2d0a88ed79049797a43a5599911
Raffaele Di Martino <r.dimartino.uk@gmail.com>
2023-02-24 19:35:39 +0000

    adding test to engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
@@ -17,3 +18,24 @@ test("Can get GitHub username via getGithub()", () => {
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
 });
+
+test("The engineer should extend the emploiee class", () => {
+  const engineer = new Engineer();
+  expect(engineer).toBeInstanceOf(Employee)
+})
+
+test("On construction, the base class is initialised", () =>{
+  
+  //arrange 
+  const name = "Bob";
+  const id = 100;
+  const email = "email@gmail.com";
+
+  //act
+  const engineer = new Engineer(name, id, email);
+
+  //assert
+  expect(engineer.name).toBe(name);
+  expect(engineer.id).toBe(id);
+  expect(engineer.email).toBe(email);
+})
